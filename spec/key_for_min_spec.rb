f5fc3642d0737cc048b34c6259edb15d94df7f3b
maxwell <maxwellbenton@gmail.com>
2021-02-19 10:10:17 -0500

    add new tests

--- spec/key_for_min_spec.rb
+++ spec/key_for_min_spec.rb
@@ -1,63 +1,18 @@
-describe "smallest hash value" do
+describe "key_for_found_value" do
 
-  it "does not call the `#keys` method" do
+  it "does not call th `#keys` method" do
     hash = {:blake => 500, :ashley => 2, :adam => 1}
 
     expect(hash).to_not receive(:keys)
-
-    key_for_min_value(hash)
+  
+    key_for_found_value(hash, 2)
   end
 
-  it "does not call the `#values` method" do
+  it "returns the first key associated with a provided value" do
     hash = {:blake => 500, :ashley => 2, :adam => 1}
 
-    expect(hash).to_not receive(:values)
-
-    key_for_min_value(hash)
-  end
-
-  it "does not call the `#min` method" do
-    hash = {:blake => 500, :ashley => 2, :adam => 1}
-
-    expect(hash).to_not receive(:min)
-
-    key_for_min_value(hash)
-  end
-
-  it "does not call the `#sort` method" do
-    hash = {:blake => 500, :ashley => 2, :adam => 1}
-
-    expect(hash).to_not receive(:sort)
-
-    key_for_min_value(hash)
-  end
-
-  it "does not call the `#sort_by` method" do
-    hash = {:blake => 500, :ashley => 2, :adam => 1}
-
-    expect(hash).to_not receive(:sort_by)
-
-    key_for_min_value(hash)
-  end
-
-  it "does not call the `#min_by` method" do
-    hash = {:blake => 500, :ashley => 2, :adam => 1}
-
-    expect(hash).to_not receive(:min_by)
-
-    key_for_min_value(hash)
-  end
-
-  it "returns the key of the smallest hash value" do
-    expect(key_for_min_value({:blake => 500, :ashley => 2, :adam => 1})).to eq(:adam)
-  end
-
-  it "returns the key of the smallest hash value example 2" do
-    expect(key_for_min_value({:blake => 10, :ashley => 50, :adam => 17})).to eq(:blake)
-  end
-
-  it "returns nil for an empty hash" do
-    expect(key_for_min_value({})).to eq(nil)
+    expect(key_for_found_value(hash, 2)).to equal(:ashley)
+    expect(key_for_found_value(hash, 500)).to equal(:blake)
   end
 
 end
